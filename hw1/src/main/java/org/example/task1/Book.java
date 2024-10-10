56b2501f362349e3bbc3e31815bef2fa408fa41d
Svail13 <svail.13@gmail.com>
2024-10-10 23:08:25 +0200

    Books done

---
+++ hw1/src/main/java/org/example/task1/Book.java
@@ -0,0 +1,49 @@
+package org.example.task1;
+/*
+Задача "Книга":
+Создайте класс Book с полями для хранения названия, автора и года издания.
+Реализуйте методы для получения и установки этих полей.
+Создайте интерфейс Displayable с методом display(), который реализуется в классе Book для отображения информации о книге.
+ */
+public class Book implements Displayable {
+
+    String name;
+    String author;
+    int year;
+
+    //setter NAME
+    public void setName (String name) {
+        this.name = name;
+    };
+    //getter NAME
+    public String getName(){
+        return this.name;
+    }
+
+    //setter AUTHOR
+    public void setAuthor(String author){
+        this.author = author;
+    };
+    //getter AUTHOR
+    public String getAuthor(){
+        return this.author;
+    };
+
+    //setter YEAR
+    public void setYear (int year){
+        this.year = year;
+    };
+    //getter YEAR
+    public int getYear (){
+        return this.year;
+    };
+
+
+
+    @Override
+    public void print() {
+        System.out.println("Name of the book: " + this.name);
+        System.out.println("Author of the book: " + this.author);
+        System.out.println("Year of the book: " + this.year);
+    }
+}
